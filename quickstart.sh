5ed85334d2f09e2b0eafce144400d7ccc7cd4fe9
Ryan Smith <ryan.michael.smith@gmail.com>
2023-06-26 15:26:37 -0400

    switch to deb for containerd

--- quickstart.sh
+++ quickstart.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
-CONTAINERD_VERSION=1.7.2
-RUNC_VERSION=1.0.2
-CNI_PLUGIN_VERSION=1.3.0
-
 set -eux
 
 apt-get update && apt-get upgrade -y
 
-apt-get install -y jq curl gnupg apt-transport-https ca-certificates vim
+apt-get install -y jq curl gnupg apt-transport-https ca-certificates
 
 
 # Forwarding IPv4 and letting iptables see bridged traffic
@@ -30,29 +26,10 @@ EOF
 # Apply sysctl params without reboot
 sysctl --system
 
-
-curl -OL https://github.com/containerd/containerd/releases/download/v$CONTAINERD_VERSION/containerd-$CONTAINERD_VERSION-linux-amd64.tar.gz
-tar Cxzvf /usr/local containerd-$CONTAINERD_VERSION-linux-amd64.tar.gz
-
-curl -O https://raw.githubusercontent.com/containerd/containerd/main/containerd.service
-mkdir -p /usr/local/lib/systemd/system/
-mv ./containerd.service /usr/local/lib/systemd/system/containerd.service
-systemctl daemon-reload
-systemctl enable --now containerd
-
-
-mkdir -p /etc/containerd
 containerd config default > /etc/containerd/config.toml
 sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
 systemctl restart containerd
-
-curl -OL https://github.com/opencontainers/runc/releases/download/v$RUNC_VERSION/runc.amd64
-install -m 755 runc.amd64 /usr/local/sbin/runc
-
-curl -OL https://github.com/containernetworking/plugins/releases/download/v$CNI_PLUGIN_VERSION/cni-plugins-linux-amd64-v$CNI_PLUGIN_VERSION.tgz
-mkdir -p /opt/cni/bin
-tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v$CNI_PLUGIN_VERSION.tgz
-
+systemctl enable containerd
 
 curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
